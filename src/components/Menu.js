a7c109953cc5fa800bcd9c23753270daf31132f8
Jonghyung <tofutree23@gmail.com>
2019-05-06 15:01:18 +0900

    ADD : menu design

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,21 @@
 import React from 'react';
 // 리액트 라우터에서 Link기능을 이용하기 위해서 필요한 부분. a태그를 이용해서 주소를 바꿔주게 되면 새로고침이 일어나게 된다.
 import { Link } from 'react-router-dom';
+import "../scss/Menu.scss"
 
 const Menu = () => {
     return (
         <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                {/* <li><a href="/">CLICK</a></li> <- 얘 쓰면 안됨. 새로고쳐짐.*/}
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/portfolio">Portfolio</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    {/* <li><a href="/">CLICK</a></li> <- 얘 쓰면 안됨. 새로고쳐짐.*/}
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/portfolio">Portfolio</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,8 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h2>This is Homepage</h2>
+                <h2>Hello, I'm Jonghyung.</h2>
+                <h4>Wanna be a front-end-web-developer.</h4>
             </div>
         );
     }
