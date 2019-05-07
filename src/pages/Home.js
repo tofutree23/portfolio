0d31c495841ebaf5cb8e30cc5923fdcec3f6d12c
Jonghyung <tofutree23@gmail.com>
2019-05-07 12:46:03 +0900

    ADD : BTN design changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
             <div className="home">
                 <h2 className="hello">Hello, I'm Jonghyung.</h2>
                 <p className="desc">I wanna be a front-end-web-developer.</p>
-                <div className="details"><button className="btn">See details</button></div>
+                <div className="details"><button className="detailBtn">Wanna know me?</button></div>
             </div>
         );
     }
