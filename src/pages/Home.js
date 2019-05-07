90c8fe209253ce44bbccd6dfcf33f87b8292d70f
Jonghyung <tofutree23@gmail.com>
2019-05-07 14:17:16 +0900

    ADD : details

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ class Home extends Component {
             <div className="home">
                 <h2 className="hello">Hello, I'm Jonghyung.</h2>
                 <p className="desc">I wanna be a front-end-web-developer.</p>
+                <p className="desc">If you want to know me more, please click the button below.</p>
                 <div className="details"><button className="detailBtn">Wanna know me?</button></div>
             </div>
         );
