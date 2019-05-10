7ff69cbe696bc741392f78223ec58c13421ba6f1
Jonghyung <tofutree23@gmail.com>
2019-05-10 13:00:56 +0900

    ADD : icons

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,14 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h2 className="about">This is about me</h2>
+        <h2 className="about">About me</h2>
+
+        <div className="pros">
+          <i class="fas fa-tachometer-alt" id="fast" />
+          <i class="fas fa-sync-alt" id="responsibility" />
+          <i class="far fa-comments" id="communicate" />
+          <i class="far fa-lightbulb" id="creative" />
+        </div>
       </div>
     );
   }
