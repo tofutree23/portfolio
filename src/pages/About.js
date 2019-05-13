a06ca36c371c0b2ae55803c19aa3a2ed926efbea
Jonghyung <tofutree23@gmail.com>
2019-05-13 19:04:09 +0900

    ADD : scss

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,13 +5,30 @@ class About extends Component {
   render() {
     return (
       <div className="aboutPage">
-        <h2 className="about">About me</h2>
-
+        <div>
+          <h2 className="about">About me</h2>
+        </div>
         <div className="pros">
-          <i className="fas fa-tachometer-alt" id="fast" />
-          <i className="fas fa-sync-alt" id="responsibility" />
-          <i className="far fa-comments" id="communicate" />
-          <i className="far fa-lightbulb" id="creative" />
+          <div className="pros-row">
+            <div className="pro-content">
+              <i className="fas fa-tachometer-alt" id="fast" />
+              <p className="title">Fast</p>
+            </div>
+            <div className="pro-content">
+              <i className="fas fa-sync-alt" id="responsibility" />
+              <p className="title">Responsive</p>
+            </div>
+          </div>
+          <div className="pros-row">
+            <div className="pro-content">
+              <i className="far fa-comments" id="communicate" />
+              <p className="title">Communicative</p>
+            </div>
+            <div className="pro-content">
+              <i className="far fa-lightbulb" id="creative" />
+              <p className="title">Creative</p>
+            </div>
+          </div>
         </div>
         <div className="skills">
           <i className="fab fa-html5" />
