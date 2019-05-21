365a830d51754bdecb9f5f9f5e64a6f023f9adfc
Jonghyung <tofutree23@gmail.com>
2019-05-21 13:03:11 +0900

    ADD : comments

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import AnalogClock from '../components/AnalogClock';
-import '../scss/Portfolio.scss';
+import AnalogClock from 'components/AnalogClock';
+import 'scss/Portfolio.scss';
 
 class Portfolio extends Component {
   render() {
     return (
-      <div>
+      <div className="portfolio">
         {/* 아날로그 시계 컴포넌트를 이곳에서도 css로 제어할 수 있도록 클래스네임을 부여한다 */}
         <AnalogClock className="clock" />
+        <h3 className="aboutclock">Current Local Time in Seoul</h3>
       </div>
     );
   }
