b9e6f96f5f3762e24e21995a8721d1ac056c1f05
Jonghyung <tofutree23@gmail.com>
2019-05-21 18:30:00 +0900

    MOD : change about

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,8 +7,9 @@ class Portfolio extends Component {
     return (
       <div className="portfolio">
         {/* 아날로그 시계 컴포넌트를 이곳에서도 css로 제어할 수 있도록 클래스네임을 부여한다 */}
+        <h4 className="aboutclock">Current Local Time in Seoul</h4>
         <AnalogClock className="clock" />
-        <h3 className="aboutclock">Current Local Time in Seoul</h3>
+        <h2 className="pofol">Portfolio</h2>
       </div>
     );
   }
