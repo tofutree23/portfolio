662c1630b8e4bee12e65d23f1f45688c54722f70
Jonghyung <tofutree23@gmail.com>
2019-05-22 13:47:55 +0900

    ADD : portfolio design

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,9 +6,6 @@ class Portfolio extends Component {
   render() {
     return (
       <div className="portfolio">
-        {/* 아날로그 시계 컴포넌트를 이곳에서도 css로 제어할 수 있도록 클래스네임을 부여한다 */}
-        <h4 className="aboutclock">Current Local Time in Seoul</h4>
-        <AnalogClock className="clock" />
         <h2 className="pofol">Portfolio</h2>
         <div className="section">
           <section className="section content dagabae">
@@ -22,6 +19,13 @@ class Portfolio extends Component {
               src="https://raw.githubusercontent.com/tofutree23/Resources/master/img/thumb/we.PNG"
               alt="we-tube, the most popular streaming service youtube's clone"
             />
+          </section>
+          <section
+            className="section content clock-section"
+            alt="Analog Clock with Vanilla JS"
+          >
+            {/* 아날로그 시계 컴포넌트를 이곳에서도 css로 제어할 수 있도록 클래스네임을 부여한다 */}
+            <AnalogClock className="clock" />
           </section>
         </div>
       </div>
