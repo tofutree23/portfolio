ade49d08cd2ecb099e7da76a24f1b8fec3b2f7e2
Jonghyung <tofutree23@gmail.com>
2019-05-28 16:19:47 +0900

    ADD : 3 icons for change contact menu

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,11 +3,32 @@ import Mailing from 'components/Mailing';
 import 'scss/Contact.scss';
 
 class Contact extends Component {
+  componentDidMount = () => {};
   render() {
     return (
       <div className="contact-page" style={{ overflowX: 'hidden' }}>
         <div className="bout">
-          <h2 className="about">Contact</h2>
+          <h2 className="contact">Contact</h2>
+        </div>
+        <div className="selection">
+          <button className="ct-btn" id="sendMail">
+            <img
+              src="https://img.icons8.com/material/48/000000/email.png"
+              alt="email"
+            />
+          </button>
+          <button className="ct-btn" id="board">
+            <img
+              src="https://img.icons8.com/ios/50/000000/billboard-filled.png"
+              alt="board"
+            />
+          </button>
+          <button className="ct-btn" id="github">
+            <img
+              src="https://img.icons8.com/windows/64/000000/github.png"
+              alt="github"
+            />
+          </button>
         </div>
         <div className="rows">
           <Mailing />
