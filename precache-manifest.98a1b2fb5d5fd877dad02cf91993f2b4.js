9ce839ace28de6c69304a8317d096b57c827c7fa
Jonghyung <tofutree23@gmail.com>
2019-05-31 14:30:46 +0900

    Updates

---
+++ precache-manifest.98a1b2fb5d5fd877dad02cf91993f2b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147d9c6fbbb08c2ff36a499bc08ebe3b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ba56d16628619bc9ab5a",
+    "url": "/portfolio/static/css/main.af7d1f7d.chunk.css"
+  },
+  {
+    "revision": "9daade128a6f521792c1",
+    "url": "/portfolio/static/js/2.554418da.chunk.js"
+  },
+  {
+    "revision": "ba56d16628619bc9ab5a",
+    "url": "/portfolio/static/js/main.d3f8159b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
