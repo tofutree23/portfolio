bc8f05948f573db7b53ae67037fe79bd88a590a3
Jonghyung <tofutree23@gmail.com>
2019-06-01 17:18:05 +0900

    Updates

--- precache-manifest.efe3a01de0a75bc62a3ac70346f9d669.js
+++ precache-manifest.efe3a01de0a75bc62a3ac70346f9d669.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6f02a0b986d348af19159a840d260ca6",
+    "revision": "01d4bca0007a242081f7ca2cc32fbdf4",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "dfc1499520f833bf9cf1",
-    "url": "/portfolio/static/css/main.f2060c30.chunk.css"
+    "revision": "75865bf86819c8563369",
+    "url": "/portfolio/static/css/main.5ab770eb.chunk.css"
   },
   {
     "revision": "9daade128a6f521792c1",
     "url": "/portfolio/static/js/2.554418da.chunk.js"
   },
   {
-    "revision": "dfc1499520f833bf9cf1",
-    "url": "/portfolio/static/js/main.6f5e5c08.chunk.js"
+    "revision": "75865bf86819c8563369",
+    "url": "/portfolio/static/js/main.e3fe16b7.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e4ea93b8fed69014f8f2243644d236cb.js"
+  "/portfolio/precache-manifest.efe3a01de0a75bc62a3ac70346f9d669.js"
 );
 
 self.addEventListener('message', (event) => {
