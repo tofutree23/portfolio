eacdc82729b6b943a23ab703c6972cf32b96c507
Jonghyung <tofutree23@gmail.com>
2019-06-04 13:20:54 +0900

    ADD : menu color change

--- src/shared/App.js
+++ src/shared/App.js
@@ -4,7 +4,57 @@ import { Home, Portfolio, Contact, About } from 'pages';
 import Menu from 'components/Menu';
 import Footer from 'components/Footer';
 
+const list = document.getElementsByClassName('link');
 class App extends Component {
+  componentDidMount() {
+    const item = document.querySelectorAll('a');
+    list.item('0').addEventListener('click', () => {
+      item[0].style.color = 'crimson';
+      item[1].style.color = 'black';
+      item[2].style.color = 'black';
+      item[3].style.color = 'black';
+    });
+    list.item('1').addEventListener('click', () => {
+      item[0].style.color = 'black';
+      item[1].style.color = 'crimson';
+      item[2].style.color = 'black';
+      item[3].style.color = 'black';
+    });
+    list.item('2').addEventListener('click', () => {
+      item[0].style.color = 'black';
+      item[1].style.color = 'black';
+      item[2].style.color = 'crimson';
+      item[3].style.color = 'black';
+    });
+    list.item('3').addEventListener('click', () => {
+      item[0].style.color = 'black';
+      item[1].style.color = 'black';
+      item[2].style.color = 'black';
+      item[3].style.color = 'crimson';
+    });
+    const path = window.location.pathname;
+    if (path === '/') {
+      item[0].style.color = 'crimson';
+      item[1].style.color = 'black';
+      item[2].style.color = 'black';
+      item[3].style.color = 'black';
+    } else if (path === '/about') {
+      item[0].style.color = 'black';
+      item[1].style.color = 'crimson';
+      item[2].style.color = 'black';
+      item[3].style.color = 'black';
+    } else if (path === '/works') {
+      item[0].style.color = 'black';
+      item[1].style.color = 'black';
+      item[2].style.color = 'crimson';
+      item[3].style.color = 'black';
+    } else {
+      item[0].style.color = 'black';
+      item[1].style.color = 'black';
+      item[2].style.color = 'black';
+      item[3].style.color = 'crimson';
+    }
+  }
   render() {
     return (
       <div>
